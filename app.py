17fdc1ebdef561889e77713663acaf06b16a4ada
Gleiser Hilario <gleiser.hilario@gmail.com>
2026-02-14 23:01:07 -0500

    Configuracion puerto render

--- app.py
+++ app.py
@@ -30,5 +30,8 @@ crear_base()
 def inicio():
     return render_template("formulario.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
