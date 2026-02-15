ec7a75b2f7681c2f3b3a12451202db2fed21c045
Gleiser Hilario <gleiser.hilario@gmail.com>
2026-02-14 22:12:18 -0500

    Proyecto inicial DIANORA

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+# Crear base de datos
+def crear_base():
+    conexion = sqlite3.connect("ventas.db")
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS ventas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fecha TEXT,
+            prenda TEXT,
+            cantidad INTEGER,
+            precio REAL,
+            movilidad REAL,
+            direccion TEXT,
+            estado TEXT
+        )
+    """)
+
+    conexion.commit()
+    conexion.close()
+
+crear_base()
+
+@app.route("/")
+def inicio():
+    return render_template("formulario.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
